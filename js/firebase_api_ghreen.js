8e17d032477aa2d811a7c406caa264ab7459f601
Kevin <kevinyuanderchang@gmail.com>
2021-05-10 22:26:05 -0700

    firebase details added

---
+++ js/firebase_api_ghreen.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDrdcz9FEGzuEvO2zrp5SYt5EKlP4lVnk4",
+    authDomain: "ghreen-b21f7.firebaseapp.com",
+    projectId: "ghreen-b21f7",
+    storageBucket: "ghreen-b21f7.appspot.com",
+    messagingSenderId: "911403778708",
+    appId: "1:911403778708:web:e718bb79a7706a82442a81"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
