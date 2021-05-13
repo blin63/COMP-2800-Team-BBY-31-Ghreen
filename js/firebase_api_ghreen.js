3a7d2a5fadca50814a827acfa8f844d04f93bc52
Kevin <kevinyuanderchang@gmail.com>
2021-05-13 12:10:35 -0700

    pages all linked so far

--- js/firebase_api_ghreen.js
+++ js/firebase_api_ghreen.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
   
->>>>>>> baseline_questionnaire
   // Ghreen's Firebase configuration
   var firebaseConfig = {
     apiKey: "AIzaSyDrdcz9FEGzuEvO2zrp5SYt5EKlP4lVnk4",
@@ -13,9 +10,5 @@
   };
 
   // Initialize Firebase
-<<<<<<< HEAD
   firebase.initializeApp(firebaseConfig);
   const db = firebase.firestore();
-=======
-  firebase.initializeApp(firebaseConfig);
->>>>>>> baseline_questionnaire
--- js/firebase_login.js
+++ js/firebase_login.js
@@ -1,10 +1,5 @@
 var login = new firebaseui.auth.AuthUI(firebase.auth());
 
-var oldUser = 'main.html'
-var newUser = 'questionnaire.html'
-
-
-
 var uiConfig = {
     callbacks: {
         signInSuccessWithAuthResult: function (authResult, redirectUrl) {
@@ -26,7 +21,7 @@ var uiConfig = {
                         email: user.email                          //with authenticated user's ID (user.uid)
                     }).then(function () {
                         console.log("New user added to firestore");
-                        window.location.assign("beginQuestionnaire.html");       //re-direct to main.html after signup
+                        window.location.assign("survey_page_0.html");       //re-direct to main.html after signup
                     })
                     .catch(function (error) {
                         console.log("Error adding new user: " + error);
@@ -42,11 +37,11 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: oldUser,   
+    signInSuccessUrl: 'main.html',   
     signInOptions: [
       firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
-    tosUrl: 'main.html',
+    tosUrl: '<your-tos-url>',
     privacyPolicyUrl: '<your-privacy-policy-url>'
   };
 
