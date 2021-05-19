da139e9b2ec3abff7b4640a12121e9fb3567580a
Kevin <kevinyuanderchang@gmail.com>
2021-05-19 14:55:51 -0700

    main page now generates the percentage change and arrow direction programatically

--- js/identify_user.js
+++ js/identify_user.js
@@ -24,28 +24,29 @@ welcome();
 * This block of code was adapted from Carly's code found here: 
 * source: https://www.notion.so/Tech-Tip-B006-How-do-I-get-the-values-of-checkboxes-and-save-to-Firestore-53516773f2e243e9a4dab0e283cf0dc7 
 */
-function calculateCarbonFootprintScore() {
+function readCarbonFootprintData() {
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             db.collection("users").doc(user.uid)
                 .get()
                 .then(function (doc) {
-                    var score = doc.data().Q0
-                              + doc.data().Q1
-                              + doc.data().Q2
-                              + doc.data().Q3
-                              + doc.data().Q4
-                              + doc.data().Q5
-                              + doc.data().Q6
-                              + doc.data().Q7
-                              + doc.data().Q8
-                              + doc.data().Q9;
+                    var score = doc.data().scoreCurrent
+                    var change = doc.data().scoreChange
                     $("#user-score").text(score);
+                    $("#change").text(change.toFixed(1) + "%");
+                    console.log("Change: " + change);
+
+                    if (change < 0) {
+                        $("#direction").append('<i class="fas fa-arrow-circle-up"></i>')
+                    } else {
+                        $("#direction").append('<i class="fas fa-arrow-circle-down"></i>')
+                    }
+
                 })
         }
     });
 }
-calculateCarbonFootprintScore();
+readCarbonFootprintData();
 
 /* Extract data end 
 * source: https://www.notion.so/Tech-Tip-B006-How-do-I-get-the-values-of-checkboxes-and-save-to-Firestore-53516773f2e243e9a4dab0e283cf0dc7 */
