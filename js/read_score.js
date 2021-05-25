bfa9ae2408598118e9c3909c5ffcc3ad46254994
Kevin <kevinyuanderchang@gmail.com>
2021-05-25 02:03:07 -0700

    added some comments

--- js/read_score.js
+++ js/read_score.js
@@ -11,6 +11,7 @@ function readCarbonFootprintData() {
 
                     score = doc.data().scoreCurrent;
 
+                    /* this makes sure that the current change is always correct */
                     db.collection("users").doc(user.uid).update({
                         scoreChange: 100 * (doc.data().scoreCurrent - doc.data().scoreOld) / (doc.data().scoreOld)
                     })
@@ -25,14 +26,14 @@ function readCarbonFootprintData() {
                         $("#change").text(change.toFixed(1) + "%");
                         $("#change").addClass("negative");
                         $("#direction").append('<i class="fas fa-arrow-circle-down"></i>')
-                    } 
+                    }
 
                     if (change == 0) {
                         $("#change").text(change.toFixed(1) + "%");
                         $("#change").addClass("negative");
                     }
-                    
-                    if (change > 0){
+
+                    if (change > 0) {
                         $("#change").text("+" + change.toFixed(1) + "%");
                         $("#change").addClass("positive");
                         $("#direction").append('<i class="fas fa-arrow-circle-up"></i>')
@@ -44,5 +45,5 @@ function readCarbonFootprintData() {
 }
 readCarbonFootprintData();
 
-/* Extract data end 
+/* Extract data end
 * source: https://www.notion.so/Tech-Tip-B006-How-do-I-get-the-values-of-checkboxes-and-save-to-Firestore-53516773f2e243e9a4dab0e283cf0dc7 */
