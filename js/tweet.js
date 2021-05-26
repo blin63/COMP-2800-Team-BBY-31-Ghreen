bac8ed7e6d257b2315736c90a23432e28b41e74c
Kevin <kevinyuanderchang@gmail.com>
2021-05-26 15:59:22 -0700

    link fixes

--- js/tweet.js
+++ js/tweet.js
@@ -3,24 +3,40 @@
 * I found these from the Twitter Developer Platform
 * Source: https://developer.twitter.com/en/docs/twitter-for-websites/javascript-api/guides/set-up-twitter-for-websites 
 */
-window.twttr = (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0],
-      t = window.twttr || {};
-    if (d.getElementById(id)) return t;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://platform.twitter.com/widgets.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  
-    t._e = [];
-    t.ready = function(f) {
-      t._e.push(f);
-    };
-  
-    return t;
-  }(document, "script", "twitter-wjs"));
+window.twttr = (function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
 
-  /* 
-  * Twitter JavaScript API end
-  * *Source: https://developer.twitter.com/en/docs/twitter-for-websites/javascript-api/guides/set-up-twitter-for-websites 
+  t._e = [];
+  t.ready = function (f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
+
+/* 
+* Twitter JavaScript API end
+* *Source: https://developer.twitter.com/en/docs/twitter-for-websites/javascript-api/guides/set-up-twitter-for-websites 
 */
+
+/* function tweet() {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      db.collection("users").doc(user.uid)
+        .get()
+        .then(function (doc) {
+          var score = doc.data().scoreCurrent;
+          var tweet = "Check out this awesome app called Ghreen! My Carbon Footprint score is " + score + "! Can you beat it?"
+          var link = "https://twitter.com/intent/tweet?text=" + tweet + ""
+          $("#tweet-post a").href = link;
+        })
+    }
+  })
+}
+tweet() */
