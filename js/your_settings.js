c5ffce586a9903c7fc7525cb89ea4b414d1842f1
Carl Magno <carlmagno007@gmail.com>
2021-05-26 12:07:31 -0700

    "Changed Change Password group from Confirm and Reset to send Password Reset email instead. Attempted to add regex to name and email; regex didn't succeed. Uploaded as is."

--- js/your_settings.js
+++ js/your_settings.js
@@ -20,26 +20,31 @@ firebase.auth().onAuthStateChanged(function(user) {
             let newName = document.getElementById("name").value;
             let newEmail = document.getElementById("email").value;
 
-            let nameRegex = '{1}[A-Z]{1,}[a-z]\s{1}[A-Z]{1,}[a-z]';
-            let emailRegex = '{1,}[A-Za-z0-9]@{1,}[a-z].{,3}[a-z]';
+            let nameRegex = new RegExp("[A-Z]{1}[a-z]{1,}[A-Z]{1}[a-z]{1,}");
+            let emailRegex = new RegExp('[A-Za-z0-9]{1,}@[a-z]{1,}.[a-z]{,3}');
 
-            user.updateProfile({
-                displayName: newName
-            }).then(function() {
-                console.log("Update successful.");
-            }).catch (function(error) {
-                console.log("Update unsuccesful: " + error);
-            });
+            if (newName.match(nameRegex)) {
+                console.log("Name Match");
+                user.updateProfile({
+                    displayName: newName
+                }).then(function() {
+                    console.log("Update successful.");
+                }).catch (function(error) {
+                    console.log("Update unsuccesful: " + error);
+                });
+            }
 
-            user.updateEmail(newEmail)
-            .then( function() {
-                console.log("Update successful.");
-            }).catch (function(error) {
-                console.log("Update unsuccesful: " + error);
-            });
+            if (newEmail.match(emailRegex)) {
+                console.log("Email Match");
+                user.updateEmail(newEmail)
+                .then( function() {
+                    console.log("Update successful.");
+                }).catch (function(error) {
+                    console.log("Update unsuccesful: " + error);
+                });
+            }
 
-            user.displayName = newName;
-            user.email = newEmail;
+
             
             return db.collection("users").doc(user.uid).update({
                 name: newName ,
@@ -73,49 +78,20 @@ firebase.auth().onAuthStateChanged(function(user) {
 
         // Confirm Password changes function
 
-        document.getElementById("confirmPassword").addEventListener("click", function() {
+        document.getElementById("resetEmail").addEventListener("click", function() {
 
-            let currentPassword = document.getElementById("currentPassword").value;
-            let newPassword = document.getElementById("newPassword").value;
-
-            console.log(user.password);
-            console.log(currentPassword);
-
-            if (currentPassword == user.password) {
-                user.updatePassword(newPassword)
-                .then( function() {
-                    console.log("Update successful.");
-                }).catch (function(error) {
-                    console.log("Update unsuccesful: " + error);
-                });
-            } else {
-                alert("Current password must match password in database.");
-            }
             
-            return db.collection("users").doc(user.uid).update({
-            }).then (function () {
+            let auth = firebase.auth();
+            let emailAddress = user.email;
 
-                console.log(user);
-                // As suggested by Carly in Slack
-                db.collection("users")
-                .doc(user.uid)
-                .get()
-                .then(function(doc) {
-
-                    console.log("After update");
-                    console.log(displayName);
-                    console.log(email);
-                    console.log(doc.data());
-
-                    document.getElementById("currentPassword").value = "";
-                    document.getElementById("newPassword").value = "";
-                })
-
-            })
-            .catch(function (error) {
-                console.log("Error updating user: " + error);
-            })
-
+            auth.sendPasswordResetEmail(emailAddress).then(function() {
+                alert("Email sent. Please check your email.");
+                console.log("Email sent.");
+              }).catch(function(error) {
+                console.log("Email not sent: " + error);
+              });
+            
+            
         });
 
     } else {
