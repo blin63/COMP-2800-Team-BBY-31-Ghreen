54d1215d1e42e97425b230e12b26f18f932165a2
Carl Magno <carlmagno007@gmail.com>
2021-05-25 17:54:44 -0700

    Removed "Settings" and gear icon from your settings. Fixed Javascript to have separate update functionality between Account Details and Change Password.

--- js/your_settings.js
+++ js/your_settings.js
@@ -6,32 +6,22 @@ firebase.auth().onAuthStateChanged(function(user) {
 
         let email = user.email;
         let userName = user.displayName;
-        let country = user.country;
 
         console.log("Before update: ")
         console.log("Name: " + userName);
         console.log("Email: " + email);
-        console.log("Country: " + country);
         console.log(user);
         
-        document.getElementById("confirm").addEventListener("click", function() {
+
+        // Confirm Account changes function
+
+        document.getElementById("confirmAccount").addEventListener("click", function() {
 
             let newName = document.getElementById("name").value;
             let newEmail = document.getElementById("email").value;
-            let newCountry = document.getElementById("country").value;
-            let currentPassword = document.getElementById("currentPassword").value;
-            let newPassword = document.getElementById("newPassword").value;
 
-            if (currentPassword == user.password) {
-                user.updatePassword(newPassword)
-                .then( function() {
-                    console.log("Update successful.");
-                }).catch (function(error) {
-                    console.log("Update unsuccesful: " + error);
-                });
-            } else {
-                alert("Current password must match password in database.");
-            }
+            let nameRegex = '{1}[A-Z]{1,}[a-z]\s{1}[A-Z]{1,}[a-z]';
+            let emailRegex = '{1,}[A-Za-z0-9]@{1,}[a-z].{,3}[a-z]';
 
             user.updateProfile({
                 displayName: newName
@@ -50,14 +40,10 @@ firebase.auth().onAuthStateChanged(function(user) {
 
             user.displayName = newName;
             user.email = newEmail;
-            user.password = newPassword;
             
             return db.collection("users").doc(user.uid).update({
                 name: newName ,
                 email: newEmail ,
-                country: newCountry,
- 
-                
             }).then (function () {
 
                 console.log(user);
@@ -67,18 +53,60 @@ firebase.auth().onAuthStateChanged(function(user) {
                 .get()
                 .then(function(doc) {
                     email = doc.data().email,
-                    country = doc.data().country,
                     displayName = doc.data().name
 
                     console.log("After update");
                     console.log(displayName);
                     console.log(email);
-                    console.log(country);
                     console.log(doc.data());
 
                     document.getElementById("name").value = "";
                     document.getElementById("email").value = "";
-                    document.getElementById("country").value = "";
+                })
+
+            })
+            .catch(function (error) {
+                console.log("Error updating user: " + error);
+            })
+
+        });
+
+        // Confirm Password changes function
+
+        document.getElementById("confirmPassword").addEventListener("click", function() {
+
+            let currentPassword = document.getElementById("currentPassword").value;
+            let newPassword = document.getElementById("newPassword").value;
+
+            console.log(user.password);
+            console.log(currentPassword);
+
+            if (currentPassword == user.password) {
+                user.updatePassword(newPassword)
+                .then( function() {
+                    console.log("Update successful.");
+                }).catch (function(error) {
+                    console.log("Update unsuccesful: " + error);
+                });
+            } else {
+                alert("Current password must match password in database.");
+            }
+            
+            return db.collection("users").doc(user.uid).update({
+            }).then (function () {
+
+                console.log(user);
+                // As suggested by Carly in Slack
+                db.collection("users")
+                .doc(user.uid)
+                .get()
+                .then(function(doc) {
+
+                    console.log("After update");
+                    console.log(displayName);
+                    console.log(email);
+                    console.log(doc.data());
+
                     document.getElementById("currentPassword").value = "";
                     document.getElementById("newPassword").value = "";
                 })
